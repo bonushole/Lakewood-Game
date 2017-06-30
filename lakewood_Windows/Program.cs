2312bdc73b00537268cb9914bb2ef4c9487710b5
Maximilian Evans <156max@gmail.com>
2017-06-30 11:27:20 -0400

    changed up the shared code scheme so that the game could be tested on windows

---
+++ lakewood_Windows/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lakewood_Windows
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Lakewood.Game1())
+                game.Run();
+        }
+    }
+#endif
+}
